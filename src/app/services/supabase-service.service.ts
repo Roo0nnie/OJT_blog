d18f7ec159300b0ad4c4711b37e9fa9092a81a16
Ronnie Estillero <ronniewarrior09@gmail.com>
2025-01-31 23:40:28 +0800

    added checkbox

--- src/app/services/supabase-service.service.ts
+++ src/app/services/supabase-service.service.ts
@@ -19,6 +19,66 @@ export class SupabaseServiceService {
       query = query.ilike('todo', `%${searchTerm}%`);
     }
 
-    return await query.order('id', { ascending: true });
+    return await query.order('created_at', { ascending: false });
+  }
+
+  async addTodo(todo: { todo: string; status: boolean }) {
+    const { data, error } = await this.supabase
+      .from('todos')
+      .insert([{ ...todo }]);
+    return { data, error };
+  }
+
+  async deleteTodo(id: number) {
+    const { data, error } = await this.supabase
+      .from('todos')
+      .delete()
+      .eq('id', id);
+
+    return { data, error };
+  }
+
+  async getTodoById(id: number) {
+    const { data, error } = await this.supabase
+      .from('todos')
+      .select('*')
+      .eq('id', id)
+      .single();
+    return { data, error };
+  }
+
+  async updateTodo(id: number, editNewTodo: { todo: string; status: boolean }) {
+    const { data, error } = await this.supabase
+      .from('todos')
+      .update({ ...editNewTodo })
+      .eq('id', id);
+    return { data, error };
+  }
+
+  async toggleStatus(id: number) {
+    try {
+      const { data: todo, error: fetchError } = await this.supabase
+        .from('todos')
+        .select('status')
+        .eq('id', id)
+        .single();
+
+      if (fetchError) {
+        throw new Error(fetchError.message);
+      }
+
+      const { data, error } = await this.supabase
+        .from('todos')
+        .update({ status: !todo.status })
+        .eq('id', id);
+
+      if (error) {
+        throw new Error(error.message);
+      }
+
+      return { data, error };
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
   }
 }
