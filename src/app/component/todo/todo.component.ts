807ceb5e3bcc23527c76b83063772560696c24b2
Ronnie Estillero <ronniewarrior09@gmail.com>
2025-01-31 15:39:06 +0800

    added search feature

--- src/app/component/todo/todo.component.ts
+++ src/app/component/todo/todo.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SupabaseServiceService } from '../../services/supabase-service.service';
 
 @Component({
   selector: 'app-todo',
@@ -8,4 +9,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
 })
-export class TodoComponent {}
+export class TodoComponent implements OnInit {
+  constructor(private supabaseService: SupabaseServiceService) {}
+
+  searchTerm: string = '';
+  todos: any[] = [];
+  errorMessage: string = '';
+
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  async getTodos() {
+    try {
+      const { data, error } = await this.supabaseService.getTodo(
+        this.searchTerm
+      );
+      if (error) {
+        this.errorMessage = error.message;
+        return;
+      }
+      this.todos = data ?? [];
+    } catch (error: any) {
+      this.errorMessage = error.message;
+    }
+  }
+}
--- src/app/services/supabase-service.service.ts
+++ src/app/services/supabase-service.service.ts
@@ -1,6 +1,24 @@
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SupabaseServiceService {}
+export class SupabaseServiceService {
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(environment.apiUrl, environment.apiKey);
+  }
+
+  async getTodo(searchTerm: string = '') {
+    let query = this.supabase.from('todos').select('*');
+
+    if (searchTerm) {
+      query = query.ilike('todo', `%${searchTerm}%`);
+    }
+
+    return await query.order('id', { ascending: true });
+  }
+}
